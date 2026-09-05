7ad2e505610320a5ea9fff850a8a3fdd46ab430d
jake <jakevm21@gmail.com>
2026-09-05 13:30:31 -0700

    fix: incorrect comment

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -15,7 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
+            { out,                            "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
@@ -27,7 +27,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
     -- Essential plugins
     'nvim-lua/plenary.nvim', -- Utility functions (dependency for many plugins)
-    
+
     -- Treesitter for syntax highlighting
     {
         'nvim-treesitter/nvim-treesitter',
@@ -47,45 +47,45 @@ require('lazy').setup({
         },
     },
 
-      -- Autocompletion system
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-      "hrsh7th/cmp-buffer",   -- Buffer source
-      "hrsh7th/cmp-path",     -- Path source
-      "L3MON4D3/LuaSnip",     -- Snippet engine
-      "saadparwaiz1/cmp_luasnip", -- Snippet source
+    -- Autocompletion system
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+            "hrsh7th/cmp-buffer", -- Buffer source
+            "hrsh7th/cmp-path",   -- Path source
+            "L3MON4D3/LuaSnip",   -- Snippet engine
+            "saadparwaiz1/cmp_luasnip", -- Snippet source
+        },
     },
-  },
 
-  -- File explorer
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
+    -- File explorer
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
 
-  -- Fuzzy finder
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
+    -- Fuzzy finder
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
 
-  -- Key binding helper
-  {
-    "folke/which-key.nvim",
-  },
+    -- Key binding helper
+    {
+        "folke/which-key.nvim",
+    },
 
     -- Autopairs
     {
-      "windwp/nvim-autopairs",
-      event = "InsertEnter", -- Lazy-load on entering insert mode
-      config = function()
-        require("nvim-autopairs").setup({
-          disable_filetype = { "TelescopePrompt", "vim" },
-          check_ts = true, -- Enable Treesitter-based pairing
-        })
-      end,
+        "windwp/nvim-autopairs",
+        event = "InsertEnter", -- Lazy-load on entering insert mode
+        config = function()
+            require("nvim-autopairs").setup({
+                disable_filetype = { "TelescopePrompt", "vim" },
+                check_ts = true, -- Enable Treesitter-based pairing
+            })
+        end,
     },
 
     -- Autoformatter
@@ -102,10 +102,10 @@ require('lazy').setup({
         }
     },
 
-  -- Theme (load last after all functionality is configured)
-  { 
-    "catppuccin/nvim", 
-    name = "catppuccin",
-    priority = 1000, -- Load last
-  },
+    -- Theme (load last after all functionality is configured)
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000, -- Load first
+    },
 })
